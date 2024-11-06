219215b4598addd046ee66b50c0d22da133d7184
stacy orwa <stacy.orwa@turaco.insure>
2024-11-06 05:32:31 +0300

    python week 1 submission

---
+++ user_input.py
@@ -0,0 +1,11 @@
+name = input("What's your name? ")   
+int(input("Please enter your age: ")) #conver to interger
+locaton = input("please enter your location: ")
+
+name = "Stacy"
+age = 22
+location = "Nairobi"
+
+#personalized question
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+
